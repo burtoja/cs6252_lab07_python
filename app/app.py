375f33e2d6b786e094dc9ee23d317417e2287363
J. Allen Burton <jburton6@my.westga.edu>
2020-03-03 13:29:29 -0500

    Initial commit

---
+++ app/app.py
@@ -0,0 +1,23 @@
+'''
+Created on Jan 20, 2020
+
+@author: CS6252
+'''
+from flask import Flask
+from synonyms import get_synonyms_from_file
+app = Flask(__name__)
+
+
+@app.route('/synonyms')
+def get_all_synonyms():
+    all_synonyms = get_synonyms_from_file()
+    return all_synonyms
+
+@app.route('/synonym/<string:word>')
+def get_synonym(word):
+    all_synonyms = get_synonyms_from_file()
+    if word in all_synonyms.keys():
+        return {word: all_synonyms[word]}
+    else: 
+        return {"message": "No synonyms available for " + word} 
+    
---
+++ app/synonyms.py
@@ -0,0 +1,11 @@
+'''
+Created on Mar 3, 2020
+
+@author: Allen Burton
+'''
+def get_synonyms_from_file():
+    all_synonyms = {"family": ["class" , "category", "folk", "kinsfolk", "fellowship"],
+        "eat": ["consume", "use up", "deplete", "exhaust", "feed"],
+        "house": ["household", "home", "firm", "mansion", "put up", "domiciliate"]}
+    return all_synonyms
+
