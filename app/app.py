171c08ac309bf97a785769cef3c03f2370bc0358
J. Allen Burton <jburton6@my.westga.edu>
2020-03-04 22:06:03 -0500

    Rough draft of functionality before adding to base functions for flask deployment

--- app/app.py
+++ app/app.py
@@ -3,7 +3,7 @@ Created on Jan 20, 2020
 
 @author: CS6252
 '''
-from flask import Flask
+'''from flask import Flask
 from synonyms import get_synonyms_from_file
 app = Flask(__name__)
 
@@ -20,4 +20,37 @@ def get_synonym(word):
         return {word: all_synonyms[word]}
     else: 
         return {"message": "No synonyms available for " + word} 
-    
+'''
+
+def updateDictionary(dictionary, key, word):
+    if key != "" and word != "":            
+        if key in thesaurus:  
+            thesaurus[key].append(word)
+            '''print("UPDATED: (" + key + ", " + word + ") is a key already" )'''
+        else:
+            thesaurus[key] = [word]
+            '''print("ADDED: (" + key.strip() + ", " + word + ") is NOT a key already" )'''
+        return True;
+    else:
+        '''print("ERROR: Missing either key or word")'''
+        return False
+ 
+try:
+    with open("../synonyms55.txt", "r") as synonym_file: 
+        thesaurus = {}  
+        for line_number, line in enumerate(synonym_file.readlines()):
+            words = line.split(",")
+            try:
+                if not updateDictionary(thesaurus, words[0].strip(), words[1].strip()) and not updateDictionary(thesaurus, words[1].strip(), words[0].strip()):
+                    line_number += 1
+                    print ("Error in line {}: Missing word ({}, {})".format(line_number, words[0].strip(), words[1].strip()) )            
+            except IndexError:
+                line_number += 1
+                print("Error in line {}: No comma ({})".format(line_number, words[0].strip()))         
+        print (thesaurus)
+except FileNotFoundError:
+    thesaurus = {} 
+    print("Error: File not found")
+except:
+    print("Error: Error reading file")
+        
--- app/synonyms.py
+++ app/synonyms.py
@@ -4,8 +4,16 @@ Created on Mar 3, 2020
 @author: Allen Burton
 '''
 def get_synonyms_from_file():
+    synonym_file = open("../synonyms.txt", "r")
+
+    
+        
+    
+    
+    '''
     all_synonyms = {"family": ["class" , "category", "folk", "kinsfolk", "fellowship"],
         "eat": ["consume", "use up", "deplete", "exhaust", "feed"],
         "house": ["household", "home", "firm", "mansion", "put up", "domiciliate"]}
+    '''
     return all_synonyms
 
